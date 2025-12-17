8c9c7b47e2ee5619228c965693086a6bb2a39300
bala-amypo <bala@amypo.in>
2025-12-17 05:08:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,58 @@
+package.com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import jarkarta.persistence.entity;
+import jarkarta.persistence.GenerateValue;
+import jarkarta.persistence.GenerationType;
+import jarkarta.persistence.Id;
+@entity
+public class Student{
+    @Id
+    @GenerateValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String dept;
+    private LocalDate dob;
+    private float cgpa;
+    public long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+      public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+      public long getDept(){
+        return dept;
+    }
+    public void setDept(String dept){
+        this.dept=dept;
+    }
+     public long getDob(){
+        return dob;
+    }
+    public void Dob(LocalDate dob){
+        this.dob=dob;
+    }
+     public long getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+        this.cgpa=cgpa;
+    }
+    public Student(){
+
+    }
+    public Student(Long id,String name,String dept,LocalDate dob,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.dept=dept;
+        this.dob=dob;
+        this.cgpa=cgpa;
+    }
+}
