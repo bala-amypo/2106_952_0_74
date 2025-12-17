33015ac23112b418f2589a56236d85e6392c48be
bala-amypo <bala@amypo.in>
2025-12-17 04:28:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -3,5 +3,20 @@ package.com.example.demo.entity;
 import java.time.LocalDate;
 
 public class Student{
-    private Lon
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private float cgpa;
+    public long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+      public String getName(){
+        return id;
+    }
+    public void setname(Long id){
+        this.id=id;
+    }
 }
