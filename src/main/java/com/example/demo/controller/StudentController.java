03decacb83ae0734d245badc34cd137bdb535bc6
bala-amypo <bala@amypo.in>
2025-12-18 06:40:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,10 +7,9 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +20,7 @@ import com.example.demo.service.StudentService;
 public class StudentController {
     @Autowired
     StudentService stdser;
+    
     @PostMapping("/addStudent")
     public Student addStudent(@RequestBody Student st){
         return stdser.poststudent(st);
@@ -38,7 +38,8 @@ public class StudentController {
         return stdser.updateData(id,st);
     }
     @DeleteMapping("/delete/{id}")
-    public String delete(@pathVariable Long id){
-        return stdser.deleteData(id);
+    public String delete(@PathVariable Long id){
+       return stdser.deleteData(id);
     }
+
 }
