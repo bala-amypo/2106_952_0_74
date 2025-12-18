5fc0081321a60bc9302c5d0e4aedf904e916db56
bala-amypo <bala@amypo.in>
2025-12-18 04:45:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,20 +1,27 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 
 @RestController
-@RequestMapping("/students")
 public class StudentController {
-
     @Autowired
-    private StudentService studentService;
-
-    @PostMapping
-    public Student saveStudent(@RequestBody Student student) {
-        return studentService.postStudent(student);
+    StudentService stdser;
+    @PostMapping("/addStudent")
+    public Student addStudent(@RequestBody Student st){
+        return stdser.poststudent(st);
+    }
+    @GetMapping("/getall")
+    public List<Student> get(){
+        return stdser.getAllStudents();
+    }
+    @GetMapping("/getById/{id}")
+    public Optional<Student> getId(@PathVariable Long id){
+        return stdser.getById(id);
     }
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -2,23 +2,24 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
 @Service
-public class StudentServiceImpl implements StudentService {
-
-    @Autowired
+public class StudentServiceImpl implements StudentService{
+    @Autowired 
     StudentRepository stdrepo;
     @Override
     public Student poststudent(Student st){
         return stdrepo.save(st);
     }
     @Override
-    public Student postStudent(Student student) {
-        return studentRepository.save(student);
+    public List<Students> getAllStudents(){
+        return stdrepo.findAll();
     }
     @Override
     public Optional<Student> getById(Long id){
